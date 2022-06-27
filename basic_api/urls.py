f56d8a2c5306caa43db9739398a7a6e62172fe82
fob2 <finbarrobren95@gmail.com>
2022-06-27 22:39:20 +0000

    first commit

---
+++ basic_api/urls.py
@@ -0,0 +1,11 @@
+import imp
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    #path('article/', article_list),
+    path('article/', ArticleAPIView.as_view()),
+    #path('detail/<int:pk>/', article_detail),
+    path('detail/<int:id>/', ArticleDetails.as_view()),
+    path('generic/article/', GenericAPIView.as_view()),
+]
